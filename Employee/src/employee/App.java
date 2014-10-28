16c3560c3ea015092a399ad58c417a8247967fd4
dhanke2 <>
2014-10-28 11:01:46 +0100

    test

---
+++ Employee/src/employee/App.java
@@ -0,0 +1,17 @@
+package employee;
+
+
+public class App {
+
+	public static void main(String[] args) {
+
+		Employee emp1 = new Employee();
+		
+		emp1.firstName= "Peter";
+		emp1.lastName= "Müller";
+		
+		emp1.printFullName(emp1.firstName, emp1.lastName);
+
+	}
+
+}
